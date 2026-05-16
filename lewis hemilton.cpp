e9a20980006401d72cd47d0b6eb0ea30dc518703
dev-vick7 <dr7v7777777@gmail.com>
2026-05-16 11:08:05 -0700

    Updated all files

--- lewis hemilton.cpp
+++ lewis hemilton.cpp
@@ -1,28 +1,42 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
     int marks;
-    cout << "Enter your marks: ";
-    cin >> marks;
-    if (marks >= 90){
-        cout << "Grade: A";
-    }
-    else if (marks >= 75)
+    cout<<"Enter your marks: ";
+    cin>>marks;
+    switch(marks/10)
     {
-        cout << "Grade: B";
-    }
-    else if (marks >= 60)
-    {
-        cout << "Grade: C";
-    }
-    else if (marks >= 40)
-    {
-        cout << "Grade: D";
-    }
-    else
-    {
-        cout << "You are Fail";
+        case 10:
+         cout<<"Grade A";
+         break;
+
+        case 9:
+         cout<<"Grade A";
+         break;
+
+        case 8:
+         cout<<"Grade B";
+         break;
+
+        case 7:
+         cout<<"Grade B";
+         break;
+
+        case 6:
+          cout<<"Grade C";
+          break;
+
+        case 5:
+         cout<<"Grade D";
+          break;
+
+        case 4:
+          cout<<"Grade D";
+         break;
+
+        default:
+          cout<<"You are Fail";
     }
     return 0;
 }
--- practice.cpp
+++ practice.cpp
@@ -3,10 +3,29 @@ using namespace std;
 int main()
 {
     int  n;
-    cout<<"enter a number";
+    cout<<"Enter a number ";
     cin>>n;
     if(n>0){
         for(int i=0;i<=5;i++)
+        {
+            if(i%2==0)
+            {
+                cout<<i<<" is even"<<endl;
+            }
+            else
+            {
+                cout<<i<<" is odd"<<endl;
+            }
+        }
+        }
+        else if(n==0)
+        {
+            cout<<"You Entered Zero ";
+        }
+        else
+        {
+            cout<<"Negative Numbers are not allowed";
+        }
+        }
+
         
-    }
-}
