fbdbfcb9cb45e8af4d8bd44bff328f2694d169d3
dev-vick7 <dr7v7777777@gmail.com>
2026-05-21 22:18:27 +0500

    Does'nt matter yawr

--- findingmax.cpp
+++ findingmax.cpp
@@ -33,3 +33,44 @@ int main()
 
     return 0;
 }
+
+
+
+//FINDING MINIMUM AS WELL 
+
+#include <iostream>
+using namespace std;
+
+// Function to find the minimum value in an array of 5 integers
+int min(int arr[5])
+{
+    int less = arr[0];
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] < less)
+        {
+            less = arr[i];
+        }
+    }
+    cout << less << endl;
+    return less;
+}
+
+int main()
+{
+    int arr[5];
+    cout << "Enter array elements : " << endl;
+    
+    // Loop to take 5 elements from the user
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Call the function and store the result
+    int small = min(arr);
+    cout << "Minimum value is : " << small << endl;
+
+    return 0;
+}
